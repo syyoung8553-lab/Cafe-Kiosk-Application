16600d7ad5eca1543cb2d126f0bccacf58a5bc20
youngkyu <syyoung8553@gmail.com>
2026-02-03 20:25:10 +0900

    기초작업

--- CafeKioskApp/ViewController.swift
+++ CafeKioskApp/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    
+    let label = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        ConfigureUI()
     }
 
 
+    func ConfigureUI(){
+        view.backgroundColor = .white
+        label.text = "🍩스파르타 카페"
+        label.textColor = .black
+        label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        
+        view.addSubview(label)
+        
+        label.snp.makeConstraints{
+            $0.height.equalTo(50)
+            $0.centerX.equalToSuperview()
+            $0.top.equalToSuperview().offset(50)
+            
+        }
+    }
+    
+    
 }
 
