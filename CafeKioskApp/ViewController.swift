738ee41a091b7e3bce935037fccc6fbf48e939d8
youngkyu <syyoung8553@gmail.com>
2026-02-03 12:28:25 +0900

    first commit

---
+++ CafeKioskApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CafeKioskApp
+//
+//  Created by ios on 2/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
